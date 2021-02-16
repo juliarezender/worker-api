83c0e03d6ac5346478a23443ef75a708280df9fc
julia <julia@DESKTOP-15FBI5U>
2021-02-16 11:17:48 -0300

    implementando models e sua validação

---
+++ WorkerApi/Controllers/ColaboradorController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WorkerApi.Models;
+
+namespace WorkerApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ColaboradorController : ControllerBase
+    {
+
+        private readonly ILogger<ColaboradorController> _logger;
+
+        public ColaboradorController(ILogger<ColaboradorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Colaborador colaborador)
+        {
+            if (ModelState.IsValid)
+            {
+                return Ok();
+            }
+            return BadRequest(ModelState);
+        }
+    }
+}
---
+++ WorkerApi/Models/Colaborador.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WorkerApi.Models
+{
+    public class Colaborador
+    {
+        [Required]
+        public long Id { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        public string Telefone { get; set; }
+        [Required]
+        public string Email { get; set; }
+    }
+}
