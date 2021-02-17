3c4cb200dd720ca682b3f334aa1f4222b22db722
julia <julia@DESKTOP-15FBI5U>
2021-02-17 11:54:59 -0300

    implementando integração com azure storage emulator

--- WorkerApi/Controllers/ColaboradorController.cs
+++ WorkerApi/Controllers/ColaboradorController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Domain.Modelos;
+using Azure.Storage.Queues;
+using System;
+using System.Threading.Tasks;
 
 namespace WorkerApi.Controllers
 {
@@ -18,13 +21,29 @@ namespace WorkerApi.Controllers
         }
 
         [HttpPost]
-        public IActionResult DadosColaborador([FromBody] Colaborador colaborador)
+        public async Task<IActionResult> DadosColaboradorAsync([FromBody] Colaborador colaborador)
         {
             if (ModelState.IsValid)
             {
+                string connectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING");
+                QueueClient queue = new QueueClient(connectionString, "to-email"); 
+                
+                string value = string.Concat(colaborador.Nome, ", ", colaborador.Telefone, ", ", colaborador.Email);
+                await InsertMessageAsync(queue, value);
+                Console.WriteLine($"Sent: {value}");
+
                 return Ok();
             }
             return BadRequest(ModelState);
+        }
+        static async Task InsertMessageAsync(QueueClient theQueue, string newMessage)
+        {
+            if (null != await theQueue.CreateIfNotExistsAsync())
+            {
+                Console.WriteLine("The queue was created.");
+            }
+
+            await theQueue.SendMessageAsync(newMessage);
         }
     }
 }
