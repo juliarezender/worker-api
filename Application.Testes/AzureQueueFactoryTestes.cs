13adfaafed5ba70aadefc9631c1ec428de69ce89
julia <julia@DESKTOP-15FBI5U>
2021-02-22 09:58:28 -0300

    teste da classe azure queue factory

---
+++ Application.Testes/AzureQueueFactoryTestes.cs
@@ -0,0 +1,28 @@
+﻿using Application.Service;
+using Azure.Storage.Queues;
+using Moq;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace Application.Testes
+{
+    [TestFixture]
+    public class AzureQueueFactoryTestes
+    {
+        private AzureQueueFactory azureQueueFactory;
+
+        [SetUp]
+        public void Setup()
+        {
+            azureQueueFactory = new AzureQueueFactory();
+        }
+
+        [Test]
+        public void TesteRetornoDoMetodoObterQueue()
+        {
+            var retorno = azureQueueFactory.ObterQueue();
+
+            Assert.AreEqual(retorno.GetType(), typeof(QueueClient));
+        }
+    }
+}
